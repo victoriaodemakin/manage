1e68a23148a3656d6e6d29b4d8b227379ed36be1
ifeoluwa <victoriaodemakin@gmail.com>
2022-05-08 17:53:57 -0700

    loading npm pacakge

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 95%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+      fontFamily: {
+        body: ["Be Vietnam Pro"],
+        pepp: ["Ubuntu"],
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/images/illustration-intro.svg')",
+        "simplify-pattern-mobile":
+          "url('images/bg-simplify-section-mobile.svg')",
+        "simplify-pattern-desktop":
+          "url('images/bg-simplify-section-desktop.svg')",
+        "simplify-pattern-tablet":
+          "url('images/bg-simplify-section-tablet.svg')",
+        "simplify-pattern-curry": "url('images/noodles.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
